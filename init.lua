d45c482267d3610f422267d9ca0985269aabee7d
Ján Dráb <jan.drab@hyperia.sk>
2024-10-28 08:45:53 +0100

    first customization;custom init,colorscheme

--- init.lua
+++ init.lua
@@ -891,7 +891,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'php' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'php', 'javascript', 'go' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
---
+++ lua/custom/plugins/color.lua
@@ -0,0 +1,15 @@
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.o.termguicolors = true
+-- vim.cmd [[colorscheme onedark]]
+vim.o.background = 'light'
+
+vim.g.codestats_api_key = 'SFMyNTY.YW1SeVlXST0jI01UQXhNakE9.h1sqWkqkNeGjDoLurVARa1F1gBXxOshSlwivjWP-N5U'
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+vim.o.rnu=true
+
+return {}
