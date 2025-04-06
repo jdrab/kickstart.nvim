ff86bf292cd9b4c9a1d7ef1f37c82ef24cf6c7b5
Ján Dráb <jan.drab@hyperia.sk>
2025-04-06 21:28:01 +0200

    wtf

--- init.lua
+++ init.lua
@@ -923,7 +923,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
@@ -957,3 +957,4 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
--- lua/custom/plugins/bindings.lua
+++ lua/custom/plugins/bindings.lua
@@ -1,4 +1,18 @@
 -- Define a mapping to trigger the command (Shift+Alt+f)
-vim.keymap.set('n', '<A-S-f>', ':!php-cs-fixer fix % --quiet<CR>', { silent = true })
 -- vim.api.nvim_set_keymap('n', '<A-S-f>', ':!php-cs-fixer fix % --quiet<CR>', { silent = true })
+vim.keymap.set('n', '<A-S-f>', function()
+  vim.fn.jobstart({ 'php-cs-fixer', 'fix', vim.fn.expand '%', '--quiet' }, {
+    detach = true,
+    on_exit = function()
+      vim.schedule(function()
+        vim.cmd 'e!' -- Reload the buffer to reflect changes
+      end)
+    end,
+  })
+end, { silent = true })
+
+-- vim.keymap.set('n', '<A-S-f>', ':!php-cs-fixer fix % --quiet<CR>', { silent = true })
+vim.keymap.set('n', '<C-Space>', ':bnext<CR>', { silent = false, desc = 'Next buffer' })
+vim.keymap.set('n', '<C-S-Space>', ':bprevious<CR>', { silent = false, desc = 'Previous buffer' })
+
 return {}
